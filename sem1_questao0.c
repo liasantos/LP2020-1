0d746ab8effd5ca97be16b5b50c52c9ffcf6950c
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-03-14 18:56:09 +0000

    Lista de exercicios - semana 1 - questão 0

---
+++ sem1_questao0.c
@@ -0,0 +1,20 @@
+/*
+Escreva um programa em C que receba dois números e os imprima em
+ordem crescente em uma mesma linha, separados por um espaço.
+*/
+
+//preambulo
+#include <stdio.h>
+
+int main() {
+  int num1, num2;
+  printf("Digite dois numeros inteiros:\n"); 
+  scanf("%i %i", &num1, &num2); //recebe dois inteiros
+  if(num1<num2){ //se entrada 1 for menor que entrada 2
+    printf("%i \t %i\n",num1, num2); //imprima nesta ordem
+  }else{ //se não
+    printf("%i \t %i\n",num2, num1); //imprima nesta ordem
+  }
+  
+  return 0;
+}
