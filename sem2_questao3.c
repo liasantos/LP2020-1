78a652c2eab0268fa205c8115de08ff9f1ed9302
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-03-15 00:18:59 +0000

    Enviando resolução de exercícios

--- sem2_questao3.c
+++ sem2_questao3.c
@@ -3,6 +3,7 @@ Escreva um programa em C que receba 3 valores reais correspondentes
 às medidas dos lados de um triângulo informe se é um triângulo EQUILÁTERO (três lados iguais), ISÓSCELES (dois lados iguais) ou ESCALENO (3 lados diferentes), ou ainda informe NÃO FORMA caso as medidas não formem triângulo. A condição para formação de um triângulo é que a soma das medidas menores tem que ser maior que a maior medida
 */
 
+/*
 //preambulo
 #include <stdio.h>
 
@@ -20,19 +21,20 @@ int main() {
   if(lado1+lado2>lado3 && lado1+lado3>lado2 && lado3+lado2>lado1){
     //verifica se é equilatero
     if(lado1==lado2&&lado2==lado3){ 
-    printf("EQUILÁTERO");
+    printf("\nEQUILÁTERO");
     }
     //verifica se é isósceles
     if((lado1==lado2 && lado2!=lado3) || (lado1!=lado2 && lado2==lado3) || (lado1==lado3 && lado2!=lado3)){
-      printf("ISÓSCELES");
+      printf("\nISÓSCELES");
     }
     //verifica se é escaleno
     if(lado1!=lado2 && lado2!=lado3){
-      printf("ESCALENO");
+      printf("\nESCALENO");
     }    
   }
   else{
-    printf("NÃO FORMA");
+    printf("\nNÃO FORMA");
   }
   return 0;
   }
+  */
---
+++ sem2_questao4.c
@@ -0,0 +1,36 @@
+/*
+Em uma universidade federal, o conceito final do estudante no TCC é definida por uma banca de três professores, em que prevalece o conceito atribuído pela maioria. A avaliação é considerada inconclusiva quando não é formada uma maioria entre as notas atribuídas (ou seja, em caso de empate entre os conceitos atribuídos). Os conceitos possíveis, em ordem do conceito mais alto para o mais baixo, são: A, B, C, ou D, neste último o aluno é considerado reprovado. Escreva um programa que leia três números distintos de 1 a 4, para representar respectivamente cada um dos conceitos A, B, C, e D. O programa deve informar se o aluno foi aprovado e qual o conceito obtido, se ele foi reprovado, ou apenas se o resultado da banca é inconclusivo
+*/
+
+/*
+//preambulo
+#include <stdio.h>
+
+int main() {
+  //declaração de variaveis
+  int conceito1, conceito2, conceito3;
+  //exibe mensagem solicitando os dados de entrada
+  printf("Insira os três conceitos do aluno:\n");
+  //lê os dados de entrada
+  scanf("%i %i %i", &conceito1, &conceito2, &conceito3);
+  //condicional
+  if(conceito1!=conceito2 && conceito2!=conceito3 && conceito3!=conceito1){
+    printf("INCONCLUSIVO");
+  }
+  else{
+    if((conceito1==1 && conceito2==1)||(conceito1==1 && conceito3==1) || (conceito2==1 && conceito3==1)){
+      printf("APROVADO COM A");
+    }
+    if((conceito1==2 && conceito2==2)||(conceito1==2 && conceito3==2) || (conceito2==2 && conceito3==2)){
+      printf("APROVADO COM B");
+    }
+    if((conceito1==3 && conceito2==3)||(conceito1==3 && conceito3==3) || (conceito2==3 && conceito3==3)){
+      printf("APROVADO COM C");
+    }
+    if((conceito1==4 && conceito2==4)||(conceito1==4 && conceito3==4) || (conceito2==4 && conceito3==4)){
+      printf("REPROVADO");
+    }
+  }
+return 0;
+}
+*/
