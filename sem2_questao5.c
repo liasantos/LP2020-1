ea9274e30025bae59c8937673acdfb031d64ae91
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-03-15 02:46:41 +0000

    Enviando resolução de exercícios

---
+++ sem2_questao5.c
@@ -0,0 +1,40 @@
+/*
+Escreva um programa que receba 2 números inteiros distintos e
+apresente na tela uma lista ordenada contendo os números e seus respectivos antecessores e sucessores, sem repetição.
+*/
+
+
+// #include <stdio.h>
+
+// int main() {
+	
+// 	int num1, num2, menor, maior;
+	
+// 	printf("Digite dois numeros inteiros e distintos entre si:\n");
+// 	scanf("%i %i", &num1, &num2);
+	
+// 	if(num1 > num2){
+// 		menor = num2;
+// 		maior = num1;
+// 	}else{
+// 		menor = num1;
+// 		maior = num2;
+// 	}
+	
+// 	if( (maior - menor) == 3) {
+// 		printf("%i %i %i %i %i %i", (menor-1),(menor),(menor+1),(menor+2),(menor+3),(menor+4));
+// 	}
+	
+// 	else if( (maior - menor) == 2) {
+// 		printf("%i %i %i %i %i", (menor-1),(menor),(menor+1),(menor+2),(menor+3));
+// 	}
+	
+// 	else if( (maior - menor) == 1) {
+// 		printf("%i %i %i %i", (menor-1),(menor),(menor+1),(menor+2));
+// 	}
+// 	else{
+// 		printf("%i %i %i %i %i %i", (menor-1),(menor),(menor+1), (maior-1),(maior),(maior+1));
+// 	}
+	
+// 	return 0;
+// }
