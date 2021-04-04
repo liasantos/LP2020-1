4cc2eff3521c1de8b8b468c404aa2ea360a94a7f
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-04-04 22:26:41 +0000

    Enviando resolução de exercicios

--- sem2_questao5.c
+++ sem2_questao5.c
@@ -3,34 +3,34 @@ Escreva um programa que receba 2 números inteiros distintos e
 apresente na tela uma lista ordenada contendo os números e seus respectivos antecessores e sucessores, sem repetição.
 */
 
-//preambulo
-// #include <stdio.h>
-//declaração de variaveis
+// //preambulo
+//#include <stdio.h>
+// //declaração de variaveis
 // int main() {	
-// 	int num1, num2, menor, maior;	
-// 	printf("Digite dois numeros inteiros e distintos entre si:\n");
-// 	scanf("%i %i", &num1, &num2);	
-//coloca em ordem
-// 	if(num1 > num2){
-// 		menor = num2;
-// 		maior = num1;
-// 	}else{
-// 		menor = num1;
-// 		maior = num2;
-// 	}	
-//condicional
-// 	if( (maior - menor) == 3) {
-// 		printf("%i %i %i %i %i %i", (menor-1),(menor),(menor+1),(menor+2),(menor+3),(menor+4));
-// 	}	
-// 	else if( (maior - menor) == 2) {
-// 		printf("%i %i %i %i %i", (menor-1),(menor),(menor+1),(menor+2),(menor+3));
-// 	}	
-// 	else if( (maior - menor) == 1) {
-// 		printf("%i %i %i %i", (menor-1),(menor),(menor+1),(menor+2));
-// 	}
-// 	else{
-// 		printf("%i %i %i %i %i %i", (menor-1),(menor),(menor+1), (maior-1),(maior),(maior+1));
-// 	}
+// int num1, num2, menor, maior;	
+// printf("Digite dois numeros inteiros e distintos entre si:\n");
+// scanf("%i %i", &num1, &num2);	
+// //coloca em ordem
+// if(num1 > num2){
+// menor = num2;
+// maior = num1;
+// }else{
+// menor = num1;
+// maior = num2;
+// }	
+// //condicional
+// if( (maior - menor) == 3) {
+// printf("%i %i %i %i %i %i", (menor-1),(menor),(menor+1),(menor+2),(menor+3),(menor+4));
+// }	
+// else if( (maior - menor) == 2) {
+// printf("%i %i %i %i %i", (menor-1),(menor),(menor+1),(menor+2),(menor+3));
+// }	
+// else if( (maior - menor) == 1) {
+// printf("%i %i %i %i", (menor-1),(menor),(menor+1),(menor+2));
+// }
+// else{
+// printf("%i %i %i %i %i %i", (menor-1),(menor),(menor+1), (maior-1),(maior),(maior+1));
+// }
 	
-// 	return 0;
+// return 0;
 // }
