fa916bd363c841302daa922219850f74832a48f8
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-04-06 19:20:39 +0000

    Enviando resolução de exercícios

---
+++ sem4_questao9.c
@@ -0,0 +1,53 @@
+/* Em um dado caixa eletrônico estão disponíveis cédulas de 100, de 50,
+de 20, de 10, de 5, de 2, e de 1 real. Escreva um programa que receba um número inteiro correspondente ao valor que o cliente deseja sacar, e informe o número de notas de cada tipo que compõe a saída. Mas informe SOMENTE aquelas notas que compõem a combinação. Caso a composição tenha 0 notas daquele valor, esta não deve ser exibida.  */
+
+// #include <stdio.h>
+// int main (){
+// int valor, nota100, nota50, nota20, nota10, nota5, nota2, nota1;
+
+// scanf("%i", &valor);
+
+// nota100 = valor / 100;
+// if(nota100 > 0){
+//   printf("%i notas de R$ 100\n", nota100);
+// }
+// valor = valor % 100;
+
+// nota50 = valor / 50;
+// if(nota50 > 0){
+//   printf("%i notas de R$ 50\n", nota50);
+// }
+// valor = valor % 50;
+
+// nota20 = valor / 20;
+// if(nota20 > 0){
+//   printf("%i notas de R$ 20\n", nota20);
+// }
+// valor = valor % 20;
+
+// nota10 = valor / 10;
+// if(nota10 > 0){
+//   printf("%i notas de R$ 10\n", nota10);
+// }
+// valor = valor % 10;
+
+// nota5 = valor / 5;
+// if(nota5 > 0){
+//   printf("%i notas de R$ 5\n", nota5);
+// }
+// valor = valor % 5;
+
+// nota2 = valor / 2;
+// if(nota2 > 0){
+// printf("%i notas de R$ 2\n", nota2);
+// }
+// valor = valor % 2;
+
+// nota1 = valor / 1;
+// if(nota1 > 0){
+//   printf("%i notas de R$ 1\n", nota1);
+// }
+// valor = valor % 1;
+
+// return 0;
+// }
