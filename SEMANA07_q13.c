b88993289772a471f045fe3d140ebf959becbc9c
Lia Santos <>
2021-05-09 01:33:45 +0000

    por enquanto não funciona

---
+++ SEMANA07_q13.c
@@ -0,0 +1,27 @@
+/* Escreva um programa que leia um conjunto de números até que o valor 0 seja digitado. O programa deve apresentar na tela os três maiores números lidos em ordem decrescente. // Entrada: Uma sequência de números inteiros positivos, o último deles sendo, necessariamente, o 0. // Saída: três números inteiros, conforme especificado no enunciado. */
+
+//não funciona
+
+// #include <stdio.h>
+// int main(){
+//   int num, maior1, maior2, maior3, armazena;
+//   do{
+//     scanf("%i", &num);
+//       if(num>maior1){
+//         armazena=maior1;
+//         maior1=num;
+//         num=armazena;        
+//       }
+//       else if(num>maior2){
+//         armazena=maior2;
+//         maior1=num;
+//         num=armazena;
+//       }
+//       else if(num>maior3){
+//         armazena=maior3;
+//         maior3=num;
+//       }     
+//   } while (num!=0);  
+//   printf("%i %i %i \n", maior1, maior2, maior3);
+//   return 0;
+// }
