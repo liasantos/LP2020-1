29287fbe110b7cad60a5b1fa281989e59bbb3fc9
Lia Santos <>
2021-05-16 18:53:25 +0000

    Enviando questão 18

---
+++ SEMANA09_q18.c
@@ -0,0 +1,20 @@
+/* Escreva um programa que leia uma sequência de N números inteiros e imprima na tela a sequência na ordem invertida. // Entrada: Um número inteiro N seguido de uma sequência de N números inteiros. //Saída: Uma sequência de números inteiros impressos na tela em uma mesma linha, conforme especificado no enunciado. */
+
+// #include <stdio.h>
+// int main(){
+// int i=0, tamanho=999, sequencia[tamanho];
+
+// scanf("%i", &tamanho);
+
+// for(i=0; i<tamanho; i++){
+//   scanf("%i", &sequencia[i]);
+// }
+// //10 1 2 3 4 5 6 7 8 9 10
+// //7 -1 3 7 4 5 5 11 
+// tamanho-=1;
+// for(i=tamanho; i>=0; i--){
+// printf("%i ", sequencia[i]);
+// }
+
+// return 0;
+// }
