3a3bb41f1af39ad6909ffdbcdfe1e13368aa6b56
Lia Santos <>
2021-05-16 19:08:54 +0000

    código ainda não implementado

---
+++ SEMANA09_q19.c
@@ -0,0 +1,3 @@
+/* Escreva um programa que leia um número inteiro N e um caractere C, e imprima na tela a matriz correspondente a opção indicada por C com dimensão NxN. As opções possívels são T de matriz tabuleiro, D de matriz diagonal, S de matriz triangular superior e I de matriz triangular inferior. Note que as matrizes são binárias, e diagonal principal são sempre com todos os dígitos 1, conforme exemplo para dimensão 8x8. // Entrada: Um número inteiro N seguido de um caractere C. //Saída: uma matriz de números binários com dimensão NxN e forma especificada por C, conforme especificado no enunciado. */
+
+// ainda não implementei
