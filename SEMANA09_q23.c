3b70d1e68058f1548b736d65931467fa5c3aa729
Lia Santos <>
2021-05-17 06:26:18 +0000

    código ainda não implementado

---
+++ SEMANA09_q23.c
@@ -0,0 +1,8 @@
+/* Um dos jogos mais tradicionais nos cassinos pelo mundo é o 21. No jogo de cartas, dois jogadores jogam, e uma sequencia alternada de cartas é sorteada alternadamente para cada jogador, quando requisitado. A cada sorteio, o valor da carta é acumulado na pontuação do jogador. Ganha o jogador que fechar a soma de 21 pontos. O jogo só acaba quando ambos os jogadores decidem parar de pedir cartas. Quando um jogador para, as demais cartas da sequência devem ser distribuídas para o outro jogador. Quando um jogador atinge 21 pontos é natural que esse decida parar, e duas opções restam ao segundo jogador: seguir pedindo cartas a fim de tentar empatar o jogo, ou parar, admitindo a derrota. O empate ocorre quando ambos os jogadores atingem 21 pontos. O primeiro a estourar os 21 pontos, perde. As cartas numéricas valem seu valor respectivo, e as demais cartas Ás (A), Valete (J), Dama (Q) e Rei (K) valem, respectivamente, 1, 11, 12 e 13 pontos. Considere que o baralho tem infinitas cartas. //Entrada: Uma sequencia composta de valores numéricos de 2 a 10, caracteres maiúsculos A, J, Q, K, além do caractere S para informar quando o jogador decide parar de pedir cartas. O primeiro caractere sempre corresponde uma carta sorteada para o Jogador 1. //Saídas: O programa deve apresentar “JOGADOR 1 VENCEU” caso o jogador 1 vença, “JOGADOR 2 VENCEU” caso o jogador 2 vença, ou “EMPATE” caso ambos os jogadores completem exatamente 21 pontos ou caso ambos ultrapassem. */
+
+//código ainda não implementado
+
+//#include <stdio.h>
+//int main(){
+//  return 0;
+//}
