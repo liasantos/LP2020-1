387ce2d18cd0b2294e04dff76089a6b65edece1d
Lia Santos <>
2021-05-17 06:23:26 +0000

    código ainda não implementado

---
+++ SEMANA09_q22.c
@@ -0,0 +1,8 @@
+/* Como no meme do garotinho do mi-mi-mi, leia uma frase de até 50 caracteres e substitua toda vogal por ‘i’ ou por ‘I’, conforme a forma da vogal original. Considere que nenhuma vogal da entrada é acentuada. //Entrada: uma string de até 50 caracteres. //Saída: string com todas vogais substituidas por ‘i’ onde a orignal é minúscula, ou por ‘I’onde a original é maiúscula. */
+
+//ainda não implementei
+
+//#include <stdio.h>
+//int main{
+//  return 0;
+//}
