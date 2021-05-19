87f89cd950fbdbe4693c66b3d713e58a2d534cfc
Lia Santos <>
2021-05-19 19:15:17 +0000

    enviando questão 19 //não passou em todos os testes

--- SEMANA09_q21.c
+++ SEMANA09_q21.c
@@ -1,37 +1,29 @@
 /*  Escreva um programa que leia uma sequência de 100 números 0 e 1, e informe o tamanho da maior sequência contínua (de 0s ou de 1s) na entrada. // Entrada: uma sequência de 100 números inteiros 0s e 1s. //Saída: um número inteiro correspondendo ao tamanho da maior sequência contínua */
 
-// //ainda não funciona
-// //testar em cascata
+// //passou no teste do exercicio
+// //porem não consigo fazer parar de contar a seq.
+// //portanto funciona pela metade :(
+
 // #include <stdio.h>
 // int main(){
-//   int num, aux, somazero, somaum, msequencia, compara, sequencia[10];
-//   for(num=0; num<10; num++){
+//   int num, soma=1, msequencia=-1, sequencia[100];
+
+//   for(num=0; num<100; num++){
 //     scanf("%i", &sequencia[num]);
 //   }
-//   somazero=1;
-//   somaum=1;
-//   msequencia=0;
-//   for(num=0; num<10; num++){
-//     for(aux=num+1;aux<10;aux++){
-//       if(sequencia[num] == sequencia[aux] && num==0){
-//         somazero+=1;
-//         //continue;
-//       } 
-//         if(msequencia<somazero){
-//          msequencia=somazero;      
+//     for(num=1; num<100; num++){    
+//       if(sequencia[num] == sequencia[num+1]){
+//         soma+=1;
+//         //printf("%i\n", soma); //teste      
+//         if(msequencia<soma){
+//           msequencia=soma;
 //         }else{
-//           somazero=1;
-//         }if(sequencia[num] == sequencia[aux] && num==1){
-//         somaum+=1;
-//         //continue;
-//       } 
-//         if(msequencia<somaum){
-//           msequencia=somaum;      
-//         }
-//         else{
-//           somaum=1;
-//         }
-//   }} 
-//   printf("maior %i\n somazero %i\n somaum %i", msequencia, somazero, somaum);
+//         soma=0;  
+//         }                 
+//         //printf("soma\n%i\n", soma);//teste
+//       }
+//     }
+//   printf("\n%i",msequencia);      
+//   //printf("maior %i\n soma %i\n", msequencia, soma);//teste
 //   return 0;
 // }
